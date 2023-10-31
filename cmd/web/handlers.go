a130479d18f9cf0a34c41402efe696219cfc0a4f
cuongnt02 <cuong.ntc02@gmail.com>
2023-11-01 04:26:01 +0700

    using html parsing

---
+++ cmd/web/handlers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
+    ts, err := template.ParseFiles("./ui/html/pages/home.html")
+    if err != nil {
+        log.Print(err.Error());
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
+}
+
+func notes(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/notes" {
+        http.NotFound(w, r)
+        return
+    }
+    w.Write([]byte("Notes Page"))
+}
